37849a06a9aa0a95c42044c727b04af91b2001ae
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-04-27 15:54:44 +0200

    اتأكد من الpathes

--- js/sign.js
+++ js/sign.js
@@ -3,20 +3,18 @@ let selected = document.querySelectorAll(".form-check-input");
 
 let choose;
 selected.forEach((element) => {
-    element.addEventListener("change", (e) => {
-        nextButton.classList.remove("chooseNone");
-        choose = e.target.value;
-    })
-})
+  element.addEventListener("change", (e) => {
+    nextButton.classList.remove("chooseNone");
+    choose = e.target.value;
+  });
+});
 
 nextButton.addEventListener("click", () => {
-    if (choose === "signup-user") {
-        location.href = "../signup-user.html"
-    } else if (choose === "signup-partner") {
-        location.href = "../signup-partner.html"
-    }
-})
+  if (choose === "signup-user") {
+    location.href = "../signup-user.html";
+  } else if (choose === "signup-partner") {
+    location.href = "../signup-partner.html";
+  }
+});
 
-console.log(choose)
-
-
+console.log(choose);
