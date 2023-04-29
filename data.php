ced4c76a0646fb50978a23291745cdd4a89b5089
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-04-29 08:26:02 +0300

    decode

--- data.php
+++ data.php
@@ -1,12 +1,16 @@
 <?php
   $connection=mysqli_connect("localhost","root","","food");
-  $name=$_POST["name"];
-  $email=$_POST["email"];
-  $password=$_POST["password"];
-  $address=$_POST["address"];
-  $phone=$_POST["phone"];
-  $query="insert into user(name,email,password,address,phone) values('$name','$email','$password','$address','$phone')";
-  mysqli_query($connection,$query);
+  if(isset($_POST)){
+    $data=file_get_contents("php://input");
+    $user=json_decode($data,true);
+    $name=$user["name"];
+    $email=$user["email"];
+    $password=$user["password"];
+    $address=$user["address"];
+    $phone=$user["phone"];
+    $query="insert into user(name,email,password,address,phone) values('$name','$email','$password','$address','$phone')";
+    mysqli_query($connection,$query);
+  }
   $query="select * from user";
   $result=mysqli_query($connection,$query);
   $result=mysqli_fetch_assoc($result);
