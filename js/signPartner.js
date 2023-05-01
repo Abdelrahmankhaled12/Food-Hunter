afd691df6c9c3ac7a77bc01a810d0308868b4bbc
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-05-01 14:05:54 +0300

    update

--- js/signPartner.js
+++ js/signPartner.js
@@ -8,124 +8,115 @@ let inputConfirmPassword = document.getElementById("confirmPassword");
 let submit = document.getElementById("submit");
 
 let nameValue = "",
-  emailValue = "",
-  passValue = "",
-  conPassVAlue = "",
-  phoneValue = "",
-  branchesValue = 0,
-  locationValue = "";
+    emailValue = "",
+    passValue = "",
+    conPassVAlue = "",
+    phoneValue = "",
+    branchesValue = 0,
+    locationValue = "";
 
 inputName.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value.length >= 5) {
-    nameValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value.length >= 5) {
+        nameValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputEmail.addEventListener("change", (event) => {
-  if (
-    event.target.value.trim() &&
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value)
-  ) {
-    emailValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (
+        event.target.value.trim() &&
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value)
+    ) {
+        emailValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputPassword.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value.length >= 8) {
-    passValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value.length >= 8) {
+        passValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputConfirmPassword.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value === passValue) {
-    conPassVAlue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value === passValue) {
+        conPassVAlue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputLocation.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value.length >= 10) {
-    locationValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value.length >= 10) {
+        locationValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputBranchse.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value.length > 0) {
-    branchesValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value.length > 0) {
+        branchesValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 inputPhone.addEventListener("change", (event) => {
-  if (event.target.value.trim() && event.target.value.length === 11) {
-    phoneValue = event.target.value;
-    event.target.classList.remove("false");
-    event.target.classList.add("true");
-  } else {
-    event.target.classList.remove("true");
-    event.target.classList.add("false");
-  }
+    if (event.target.value.trim() && event.target.value.length === 11) {
+        phoneValue = event.target.value;
+        event.target.classList.remove("false");
+        event.target.classList.add("true");
+    } else {
+        event.target.classList.remove("true");
+        event.target.classList.add("false");
+    }
 });
 
 submit.addEventListener("click", (e) => {
-  if (
-    !(
-      nameValue.trim() &&
-      emailValue.trim() &&
-      passValue.trim() &&
-      locationValue.trim() &&
-      conPassVAlue.trim() &&
-      branchesValue > 0 &&
-      conPassVAlue === passValue &&
-      phone.trim()
-    )
-  ) {
+    if (nameValue.trim() && emailValue.trim() && passValue.trim() && locationValue.trim() && conPassVAlue.trim() && branchesValue > 0 && conPassVAlue === passValue && phoneValue.trim()) {
+        var data = {
+            name: nameValue,
+            email: emailValue,
+            password: passValue,
+            phone: phoneValue,
+            address: locationValue
+        };
+        var jsonData = JSON.stringify(data);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost/footer-hunter/php/implementation/signUpPartner.php", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.send(jsonData);
+        location.href = "../pages/partner/partner.html";
+}else {
     e.preventDefault();
-  }
-  fetch(
-    "http://localhost/project/Footer-hunter/php/implementation/signUpPartner.php",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: {
-        name: "abdo",
-        email: "abdo@gmail.com",
-        password: "2235",
-        address: "1oo",
-        phone: "01148687285",
-      },
-    }
-      .then((res) => res.json())
-      .then((res) => console.log(res))
-  );
+}
 });
+
