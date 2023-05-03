8c61b3110601cae8eff2fe1c5e75e4e83786b28b
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-05-03 14:43:20 +0300

    commit

--- implementation/signUpPartner.php
+++ implementation/signUpPartner.php
@@ -1,15 +1,15 @@
 <?php
-  require("../models/partner.php");
-  require("../controllers/partnercontroller.php");
+  require_once("../models/partner.php");
+  require_once("../controllers/partnercontroller.php");
     if(isset($_POST)){
     $data=file_get_contents("php://input");
     $user=json_decode($data,true);
-    $logo=$_FILES=[$user["image"]]["tmp_name"];
+    $logo=$_FILES[$user["image"]]["tmp_name"];
     $logoName=$_FILES[$user["image"]]["name"];
     $logo=base64_encode(file_get_contents(addslashes($logo)));
-    $partner= new restaurant;
+    $partner= new Custom\Partner;
     $partner->__construct2($user["email"],$user["password"],$user["name"],$user["address"],$user["phone"],$logoName,$logo,$user["open"],$user["close"],$user["fees"],$user["time"],$user["status"],$user["description"],$user["minorder"]);
-    $pcontro=new pcontrol($partner);
+    $pcontro=new restaurant($partner);
     $email=$partner->getEmail();
     $password=$partner->getPassword();
     $name=$partner->getName();
--- models/partner.php
+++ models/partner.php
@@ -1,171 +1,173 @@
 <?php
-    class restaurant{
+    namespace Custom;
+class Partner {
     private $id;
     private $email;
     private $password;
     private $name;
     private $location;
     private $phone;
+    private $logoName;
+    private $logo;
+    private $open;
+    private $close;
+    private $fees;
+    private $time;
+    private $status;
+    private $description;
+    private $minorder;
+    private $roleid = 2;
 
-	private $logoName;
+    // Empty constructor
+    public function __construct1() {}
 
-	private $logo;
-
-	private $open;
-
-	private $close;
-
-	private $fees;
-
-	private $time;
-	private $status;
-
-	private $description;
-	private $minorder;
-	private $roleid=2;
-
-    public function __construct1(){
-
+    // Parameterized constructor
+    public function __construct2($email, $password, $name, $location, $phone, $logoName, $logo, $open, $close, $fees, $time, $status, $description, $minorder) {
+        $this->email = $email;
+        $this->password = $password;
+        $this->name = $name;
+        $this->location = $location;
+        $this->phone = $phone;
+        $this->logoName = $logoName;
+        $this->logo = $logo;
+        $this->open = $open;
+        $this->close = $close;
+        $this->fees = $fees;
+        $this->time = $time;
+        $this->status = $status;
+        $this->description = $description;
+        $this->minorder = $minorder;
     }
 
-		public function __construct2($email, $password, $name, $location, $phone, $logoName, $logo, $open, $close, $fees, $time, $status, $description, $minorder, $id = 0) {
-    $this->id = $id;
-    $this->email = $email;
-    $this->password = $password;
-    $this->name = $name;
-    $this->location = $location;
-    $this->phone = $phone;
-    $this->logoName = $logoName;
-    $this->logo = $logo;
-    $this->open = $open;
-    $this->close = $close;
-    $this->fees = $fees;
-    $this->time = $time;
-    $this->status = $status;
-    $this->description = $description;
-    $this->minorder = $minorder;
-}
+    // Getter and Setter methods
+    public function getId() {
+        return $this->id;
+    }
 
+    public function setId($id) {
+        $this->id = $id;
+    }
 
-	public function getPassword() {
-		return $this->password;
-	}
-	public function setPassword($password) {
-		$this->password = $password;
-	}
+    public function getEmail() {
+        return $this->email;
+    }
 
+    public function setEmail($email) {
+        $this->email = $email;
+    }
 
-	public function getEmail() {
-		return $this->email;
-	}
-	public function setEmail($email){
-		$this->email = $email;
-	}
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
 
     public function getName() {
-		return $this->name;
-	}
-	public function setName($name) {
-		$this->name = $name;
-	}
+        return $this->name;
+    }
 
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getLocation() {
+        return $this->location;
+    }
 
-	public function getLocation() {
-		return $this->location;
-	}
-	public function setLocation($location) {
-		$this->location = $location;
-	}
+    public function setLocation($location) {
+        $this->location = $location;
+    }
 
+    public function getPhone() {
+        return $this->phone;
+    }
 
-	public function getPhone() {
-		return $this->phone;
-	}
-
-	public function setPhone($phone){
-		$this->phone = $phone;
-	}
-
-	public function getId() {
-		return $this->id;
-	}
-	public function setId($id){
-		$this->id = $id;
-	}
-
-	public function getRoleid() {
-		return $this->roleid;
-	}
-
-	public function setLogoName($logoName) {
-        $this->logoName = $logoName;
+    public function setPhone($phone) {
+        $this->phone = $phone;
     }
 
     public function getLogoName() {
         return $this->logoName;
     }
 
-    public function setLogo($logo) {
-        $this->logo = $logo;
+    public function setLogoName($logoName) {
+        $this->logoName = $logoName;
     }
 
     public function getLogo() {
         return $this->logo;
     }
 
-    public function setOpen($open) {
-        $this->open = $open;
+    public function setLogo($logo) {
+        $this->logo = $logo;
     }
 
     public function getOpen() {
         return $this->open;
     }
 
-    public function setClose($close) {
-        $this->close = $close;
+    public function setOpen($open) {
+        $this->open = $open;
     }
 
     public function getClose() {
         return $this->close;
     }
 
-    public function setFees($fees) {
-        $this->fees = $fees;
+    public function setClose($close) {
+        $this->close = $close;
     }
 
     public function getFees() {
         return $this->fees;
     }
 
-    public function setTime($time) {
-        $this->time = $time;
+    public function setFees($fees) {
+        $this->fees = $fees;
     }
+  
+	public function getTime() {
+		return $this->time;
+	}
+	
+	public function setTime($time){
+		$this->time = $time;
+		return $this;
+	}
 
-    public function getTime() {
-        return $this->time;
-    }
+	public function getStatus() {
+		return $this->status;
+	}
+	
+	public function setStatus($status): self {
+		$this->status = $status;
+		return $this;
+	}
 
-    public function setStatus($status) {
-        $this->status = $status;
-    }
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	public function setDescription($description){
+		$this->description = $description;
+		return $this;
+	}
 
-    public function getStatus() {
-        return $this->status;
-    }
-
-    public function setDescription($description) {
-        $this->description = $description;
-    }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function setMinOrder($minorder) {
-        $this->minorder = $minorder;
-    }
-
-    public function getMinOrder() {
-        return $this->minorder;
-    }
+	public function getMinorder() {
+		return $this->minorder;
+	}
+	
+	public function setMinorder($minorder){
+		$this->minorder = $minorder;
+		return $this;
+	}
+	public function getRoleid() {
+		return $this->roleid;
+	}
+	
+	public function setRoleid($roleid){
+		$this->roleid = $roleid;
+		return $this;
+	}
 }
