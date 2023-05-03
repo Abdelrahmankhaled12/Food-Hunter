f15d13de6a2b9f7887b683d536065150888bfa54
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-05-03 21:26:09 +0300

    change

--- implementation/signUpPartner.php
+++ implementation/signUpPartner.php
@@ -4,9 +4,9 @@
     if(isset($_POST)){
     $data=file_get_contents("php://input");
     $user=json_decode($data,true);
-    $logo=1/*$_FILES[$user["image"]]["tmp_name"]*/;
-    $logoName=1/*$_FILES[$user["image"]]["name"]*/;
-    $logo=1/*base64_encode(file_get_contents(addslashes($logo)))*/;
+    $logo=$_FILES[$user["image"]]["tmp_name"];
+    $logoName=$_FILES[$user["image"]]["name"];
+    $logo=base64_encode(file_get_contents(addslashes($logo)));
     $partner= new Custom\Partner;
     $partner->__construct2($user["email"],$user["password"],$user["name"],$user["address"],$user["phone"],$logoName,$logo,$user["open"],$user["close"],$user["fees"],$user["time"],$user["status"],$user["description"],$user["minorder"]);
     $pcontro=new restaurant($partner);
@@ -25,15 +25,8 @@
     $description=$partner->getDescription();
     $minOrder=$partner->getMinOrder();
     $role=$partner->getRoleid();
-    $query="insert into partner(email,password,name,location,phone,open,close,fees,time,status,description,minorder) VALUES ('$email','$password','$name','$address','$phone','$open','$close','$fees','$time','$status','$description','$minOrder')";
-    $connect=mysqli_connect("localhost","root","","foodhunter");
-    mysqli_query($connect,$query);
-    /*$operation=$pcontro->signup($query);
-    if($operation===true){
-      echo "Done";
-    }else{
-      echo "Error";
-    }*/
+    $query="insert into partner(email,password,name,location,phone,logoname,logo,open,close,fees,time,status,description,minorder) VALUES ('$email','$password','$name','$address','$phone',$logoName,$logo,'$open','$close','$fees','$time','$status','$description','$minOrder')";
+    $operation=$pcontro->signup($query);
+    $query="insert into role(email,password,roleid) values('$email','$password','$role')";
+    $pcontro->signup($query);
     }
-    /*$query="insert into role(email,password,roleid) values('$email','$password','$role')";
-    $pcontro->signup($query);*/
