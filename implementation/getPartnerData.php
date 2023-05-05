7fed529810869d4cdaed180a42a9c4622938f5e8
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-05-05 15:36:26 +0300

    v

--- implementation/getPartnerData.php
+++ implementation/getPartnerData.php
@@ -3,11 +3,7 @@
     require_once("../controllers/partnercontroller.php");
     if(isset($_GET)){
       $partner = new Custom\Partner;
-<<<<<<< HEAD
       $partner->setEmail("shehab@gmail.com");
-=======
-      $partner->setEmail($_GET["email"]);
->>>>>>> d46aaba835a61e7759f7a73d73a0719ff4691456
       $pcontro=new restaurant($partner);
       $rows=$pcontro->getData();
       $imageData = $rows[0]['logo'];
