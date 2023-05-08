f8b23255362c395c09ec3df883602459997a7278
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-05-08 18:04:46 +0300

    commit

--- implementation/signUpDelivery.php
+++ implementation/signUpDelivery.php
@@ -1,6 +1,7 @@
 <?php
   require_once("../models/delivery.php");
   require_once("../controllers/dbcontroller.php");
+  require_once("../controllers/deliverycontroller.php");
   if(isset($_POST)){
     $delivery=new delivery;
     $delivery->__construct2($_POST["email"],$_POST["password"],$_POST["name"],$_POST["phone"],$_POST["license"]);
