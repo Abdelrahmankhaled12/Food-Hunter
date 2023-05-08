e9cad1ca7eb72f1add9873f6ca9b9255eb41f595
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-05-08 17:55:36 +0300

    commit

--- implementation/makeOrder.php
+++ implementation/makeOrder.php
@@ -7,7 +7,7 @@
     $id=$_POST["userid"];
     $user->setId($id);
     $order=new order;
-    $order->__construct2("1",$user->getId(),"none",$_POST["orderdetails"],$_POST["price"],$_POST["partnerid"],$_POST["feedback"],$_POST["ratings"],$_POST["review"]);
+    $order->__construct2("1",$user->getId(),"none",$_POST["orderdetails"],$_POST["price"],$_POST["partnerid"],$_POST["feedback"],$_POST["ratings"],$_POST["review"],$_POST["orderTime"],"none");
     $usercontro=new usercontro;
     $usercontro->__construct1();
     $usercontro->setOrder($order);
