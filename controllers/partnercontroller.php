714e6c4bdc81d30d3fc43ada9026b1520722ddb8
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-05-09 15:00:48 +0300

    commit

--- controllers/partnercontroller.php
+++ controllers/partnercontroller.php
@@ -98,7 +98,7 @@
     public function getOrders(){
       $this->db=new DBController;
       $id=$this->partner->getId();
-      $query="SELECT delivers.orderid,delivers.orderdetails,delivers.price,delivers.feedback,delivers.ratings,delivers.review,user.email,user.name,user.phone,user.location from delivers INNER join user on delivers.userid=user.id WHERE delivers.partnerid='$id'";
+      $query="SELECT delivers.orderid,delivers.orderdetails,delivers.totalPrice,delivers.feedback,delivers.ratings,delivers.review,user.email,user.name,user.phone,user.location from delivers INNER join user on delivers.userid=user.id WHERE delivers.partnerid='$id'";
       $result=$this->db->select($query);
             if($result){
               $rows = array();
